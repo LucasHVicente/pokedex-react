23376a7dcb5518129da49d417770e703ab7e12bb
Lucas Vicente <l.vicente9003@gmail.com>
2023-03-14 10:34:40 -0300

    fix: pokemon evolution chain not working correctly

--- src/components/PokemonEvolutions/index.tsx
+++ src/components/PokemonEvolutions/index.tsx
@@ -24,7 +24,7 @@ export const PokemonEvolutions = ({ pokemon }: PokemonEvolutionsProps) => {
       );
     }
     setEvolutions(evo);
-  }, []);
+  }, [pokemon]);
 
   return (
     <EvolutionsContainer>
